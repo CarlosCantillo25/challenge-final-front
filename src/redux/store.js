4f270486b3bd43e137a4d15521068b21074c4000
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-08-07 17:51:46 -0300

    actualizacion

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import productReducer from "./reducers/product";
 import productsReducer from "./reducers/productsReducer";
 
 
 
 export const store = configureStore({
     reducer: {
-
-        products:productsReducer
-
+    products:productsReducer,
+      product:productReducer
       },
 })
