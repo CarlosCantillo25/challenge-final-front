5fb68f20b73d6ca83b5d84b66cb00d701e60ccb9
Donato Zollo <davinazarra@gmail.com>
2023-08-09 12:49:08 -0300

    addProduct

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -230,9 +230,9 @@ useEffect(() => {
           </div>
         ) : isLoggedIn() && user.role === 2 ? (
           // Mostrar esto solo cuando el usuario esté logueado
-          <div className='flex flex-col items-center'>
+          <div className='flex flex-col items-center me-6'>
             <div className='hidden md:flex flex-col justify-center items-center mt-2'>
-              <button onClick={navigateToControlPanel} className='p-2 text-[white] border-2 rounded flex justify-center items-center'>
+              <button onClick={navigateToControlPanel} className='w-[120%] p-3 text-[white] border-2 rounded flex justify-center items-center'>
                 <img src={user?.photo} className="h-10 rounded-full" />
                 <p>{user?.email}</p>
               </button>
@@ -253,7 +253,7 @@ useEffect(() => {
           <svg className="w-6 h-6 text-[white]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1" />
           </svg>
-          <p className='hidden md:block text-[white]'>My shopping cart</p>
+          <p className='hidden md:block text-[white] text-center'>My shopping cart</p>
         </div>
       </div>
       {isLoggedIn() && user && user.verified === false ? (
