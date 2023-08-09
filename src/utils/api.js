398ea5e3cfda88ee4b0b10a9d96e9adfefcaa10d
Donato Zollo <davinazarra@gmail.com>
2023-08-09 13:53:59 -0300

    aaa

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,6 +6,7 @@ export const endpoints = {
     product: "products/",
     login: "user/signin",
     register: "user/register",
+    google: "user/google",
     verify: "user/verify/",
     createdProduct: "products/created",
 }
